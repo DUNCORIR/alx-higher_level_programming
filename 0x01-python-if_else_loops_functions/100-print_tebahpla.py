597d7a97995b9279f78ab0fd14b90edcf8268d42
duncorir <duncorir@mail.com>
2024-09-11 23:12:36 +0300

    alphabet

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,10 +1,15 @@
 #!/usr/bin/python3
 def print_alphabet():
-    print(''.join(
-        chr(i) if (122 - i) % 2 == 0
-        else chr(i - 32)
-        for i in range(122, 96, -1)
-    ), end='')
+    print(
+        ''.join(
+            '{}'.format(
+                chr(i) if (122 - i) % 2 == 0
+                else chr(i - 32)
+            )
+            for i in range(122, 96, -1)
+        ),
+        end=''
+    )
 
 
 print_alphabet()
