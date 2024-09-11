c92efe7b5a6fd2db5f74283bdb6db8fe66fdcb0f
duncorir <duncorir@mail.com>
2024-09-11 13:46:02 +0300

    comb3

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,9 +1,5 @@
 #!/usr/bin/python3
-for digit1 in range(10):
-    # Print each combination with proper formatting
-    for digit2 in range(digit1 + 1, 10):
-        if digit1 == 8 and digit2 == 9:
-            # Special case for the very first combination
-            print(f"{digit1:01}{digit2:01}")
-        else:
-            print(f"{digit1:01}{digit2:01}", end=', ')
+for i in range(10):
+    for j in range(i +1, 10):
+        print(f"{i}{j}", end=", "
+if i != 8 or j != 9 else "\n")
