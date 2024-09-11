6da0ce7c86b6126e70be438f7d25136dc7740aba
duncorir <duncorir@mail.com>
2024-09-11 19:48:07 +0300

    magic calc

--- 0x00-python-hello_world/102-magic_calculation.py
+++ 0x00-python-hello_world/102-magic_calculation.py
@@ -3,6 +3,6 @@ def magic_calculation(a, b):
     return 98 + (a ** b)
 
 
-print(magic_calculation(2, 4))
-print(magic_calculation(6, 5))
-print(magic_calculation(8, 7))
+print("Result ({}, {}) is: {}".format(2, 4, magic_calculation(2, 4)))
+print("Result ({}, {}) is: {}".format(6, 5, magic_calculation(6, 5)))
+print("Result ({}, {}) is: {}".format(8, 7, magic_calculation(8, 7)))
