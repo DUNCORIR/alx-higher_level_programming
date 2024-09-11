bb4ad5386b458244bcb7802f2b82ebd66c13f92c
duncorir <duncorir@mail.com>
2024-09-11 15:14:42 +0300

    comb3

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 for i in range(10):
-    for j in range(i +1, 10):
-        print(f"{i}{j}", end=", "
-if i != 8 or j != 9 else "\n")
+    for j in range(i + 1, 10):
+        if i != 8 or j != 9:
+            print("{:01}{:01}".format(i, j), end=", ")
+        else:
+            print("{:01}{:01}".format(i, j))
