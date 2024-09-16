49f7b36263e12779da685ba524294b1ef2ec3e34
duncorir <duncorir@mail.com>
2024-09-16 15:32:27 +0300

    matrix

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,17 +1,6 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
-    # Check if matrix is empty or contains only empty lists
-    if matrix == [[]]:
-        return
-    # Loop through each row in the matrix
+    # Check if the matrix contains any rows to print
     for row in matrix:
-        # Create a variable to store the formatted row
-        formatted_row = ""
-        # Loop through each element in the row
-        for idx, element in enumerate(row):
-            if idx != len(row) - 1:
-                formatted_row += "{} ".format(element)
-            else:
-                formatted_row += "{}".format(element)
-    # Print the formatted row
-    print(formatted_row)
+        # Use ' '.join() to print the integers on the same line
+        print(" ".join("{:d}".format(element) for element in row))
