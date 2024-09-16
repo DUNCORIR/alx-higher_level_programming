d47df69eb2340da1e85c515f8506605b243d1caf
duncorir <duncorir@mail.com>
2024-09-16 14:36:13 +0300

    no_c

---
+++ 0x03-python-data_structures/5-no_c.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def no_c(my_string):
+    result = ""
+    # Iterate through each character in the input string
+    for character in my string:
+        # If the character is not 'c' or 'C', add it to the result
+        if character != 'c' and characer != 'C':
+            result = result + character  # Append the character to result
+    return result  # Return the resulting string
