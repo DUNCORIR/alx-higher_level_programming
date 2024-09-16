fd135c1b0ea90f4fb146c45423e1120f73dc47ee
duncorir <duncorir@mail.com>
2024-09-16 09:27:06 +0300

    find element

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -2,7 +2,7 @@
 def replace_in_list(my_list, idx, element):
     if idx < 0:  # Check if the index is negative
         return my_list
-    if idx >= len(my_list): #  Check if the index is out of range
+    if idx >= len(my_list):  # Check if the index is out of range
         return my_list
-    my_list[idx] = element  # If index is valid, replace  element at given index
+    my_list[idx] = element  # If index is valid,rep element at given index
     return my_list
