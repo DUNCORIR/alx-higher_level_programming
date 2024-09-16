2a6ca82ed1d16bf0a46e6729c83e6a5d68564261
duncorir <duncorir@mail.com>
2024-09-16 09:12:28 +0300

    find element

---
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def replace_in_list(my_list, idx, element):
+    if idx < 0:  # Check if the index is negative
+        return my_list
+    if idx >= len(my_list): #  Check if the index is out of range
+        return my_list
+    my_list[idx] = element  # If index is valid, replace  element at given index
+    return my_list
