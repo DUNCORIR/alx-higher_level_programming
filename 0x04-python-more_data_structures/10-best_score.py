f069ac7963b131ebb1ac0fa152c284dac65a0f29
duncorir <duncorir@mail.com>
2024-09-18 23:09:43 +0300

    best score

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -10,6 +10,4 @@ def best_score(a_dictionary):
         if value > best_value:
             best_value = value  # If current value greater than best_value
             best_value = key  # Update best_key to the current key
-
-
-return best_key  # Return the key with the best score
+    return best_key  # Return the key with the best score
