7cbfa34f63c3e58927750e493bb16668df58d44b
duncorir <duncorir@mail.com>
2024-09-30 13:12:11 +0300

    print int

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -3,5 +3,5 @@ def safe_print_integer(value):
     try:
         print("{:d}".format(value))
         return True  # Value is an integer and printed successfully
-    except ValueError or TypeError:
+    except (ValueError, TypeError):
         return False  # The value is not integer or printing failed.
