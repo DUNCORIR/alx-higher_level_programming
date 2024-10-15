d392f390844e01359b1675b8cb3e13acd086c5b7
duncorir <duncorir@mail.com>
2024-10-15 15:15:30 +0300

    write file

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -18,5 +18,5 @@ def write_file(filename="", text=""):
     """
 
     with open(filename, 'w', encoding='utf-8') as file:
-        num_characters_written = file.write(text) # Write text to file
-    return num_characters_written # Return number of charcters.
+        num_characters_written = file.write(text)  # Write text to file
+    return num_characters_written  # Return number of charcters.
