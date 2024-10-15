2b2db5d744b31a2eacb8eaac3a1ef89098e22568
duncorir <duncorir@mail.com>
2024-10-15 15:13:20 +0300

    write file

---
+++ 0x0B-python-input_output/1-write_file.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""
+The function that writes a string to a textfile UTD-8
+"""
+
+
+def write_file(filename="", text=""):
+    """
+    The function that writes a string to a text file (UTF8)
+    and returns the number of characters written.
+
+    Args:
+        filename (Str): The string to be written to file.
+        text(str): file to be written .
+
+    Returns:
+        int: The number of characters written.
+    """
+
+    with open(filename, 'w', encoding='utf-8') as file:
+        num_characters_written = file.write(text) # Write text to file
+    return num_characters_written # Return number of charcters.
