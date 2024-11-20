7de65468cab5a0de8d778cf07b4ef93a52f1aeaa
duncorir <duncorir@mail.com>
2024-11-20 20:33:23 +0300

    cities by states

--- 0x0E-SQL_more_queries/9-cities_by_state_join.sql
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -5,8 +5,15 @@
 USE hbtn_0d_usa;
 
 -- Query to list cities with their state names
-SELECT cities.id, cities.name, states.name
-FROM cities
-INNER JOIN states
-ON cities.state_id = states.id
-ORDER BY cities.id ASC;
+SELECT 
+	cities.id,
+	cities.name,
+	states.name
+FROM 
+	cities
+INNER JOIN 
+	states
+ON 
+	cities.state_id = states.id
+ORDER BY 
+	cities.id;
