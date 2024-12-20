4b37f89289f0a38ac04d8bfba35c274cde618bce
duncorir <duncorir@mail.com>
2024-12-20 08:49:47 +0300

    Rectangle 4

--- 0x13-javascript_objects_scopes_closures/2-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -2,15 +2,16 @@
 
 // The script for a class rectangle that defines Rectangle.
 class Rectangle {
-  constructor (w, h) {
-    // Check if w or h is 0 or not a positive integer
-    if (w <= 0 || h <= 0 || !Number.isInteger(w) || !Number.isInteger(h)) {
-      // if condition is met, create empty object by returning early
-      return {};
+    constructor(w, h) {
+        // Check if w or h is 0 or not a positive integer
+        if (w <= 0 || h<= 0 || !Number.isInteger(w) || !Number.isInteger(h)) {
+            // if condition is met, create empty object by returning early
+            return;
+        }
+        // Otherwise initialize object with given width and height.
+        this.width = w;
+        this.height = h;
     }
-    this.width = w;
-    this.height = h;
-  }
 }
 
 module.exports = Rectangle;
