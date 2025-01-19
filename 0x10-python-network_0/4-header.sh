e95128dda5fa2d05ff4984f86f5bd4d567f67683
duncorir <duncorir@mail.com>
2025-01-19 16:01:09 +0300

    header

--- 0x10-python-network_0/4-header.sh
+++ 0x10-python-network_0/4-header.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # This script sends a GET request to the URL with a custom header X-School-User-Id: 98 and displays the body of the response.
-curl -s -H "X-School-User-Id: 98" "$1"
+curl -sH "X-School-User-Id: 98" "$1"
